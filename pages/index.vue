734833ea7656a8ca4e648e846ed47279b227e19d
John Lucas <joohnzay@gmail.com>
2023-07-06 17:49:32 -0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -20,40 +20,39 @@ export default {
 </script>
 
 <style scoped>
-  h1 {
-    font-size: 2rem;
-  }
+h1 {
+  font-size: 2rem;
+}
 
-  span {
-    color: #2d6cea;
-  }
+span {
+  color: #2d6cea;
+}
 
-    a {
-        color:
-            #000;
-        width: max-content;
-        position: relative;
-        margin: 1rem 0;
-        font-size: 1.5rem;
-        font-weight: bold;
-    }
+a {
+  color:
+    #000;
+  width: max-content;
+  position: relative;
+  margin: 1rem 0;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
 
-    a:before {
-        content: '';
-        background-color: #2d6cea;
-        width: 105%;
-        height: 30%;
-        position: absolute;
-        bottom: 1px;
-        left: 50%;
-        transform: translatex(-50%);
-        z-index: -1;
-        transition: height 0.1s linear;
-        opacity: 0.4;
-    }
+a:before {
+  content: '';
+  background-color: #2d6cea;
+  width: 105%;
+  height: 30%;
+  position: absolute;
+  bottom: 1px;
+  left: 50%;
+  transform: translatex(-50%);
+  z-index: -1;
+  transition: height 0.1s linear;
+  opacity: 0.4;
+}
 
-    a:hover:before {
-        height: 100%;
-    }
-
+a:hover:before {
+  height: 100%;
+}
 </style>
